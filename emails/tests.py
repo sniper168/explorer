91350af94786ec1760c28e8d0cfc5861cf3028ca
sniper168 <sniper168888@gmail.com>
2019-11-11 14:19:15 +0800

    Update tests.py

--- emails/tests.py
+++ emails/tests.py
@@ -1,3 +1,55 @@
 from django.test import TestCase
 
 # Create your tests here.
+{
+  "block_hash": "0000000000000000000fd91b517121289f96164aa8dd5a3c6da238cb9be459fc",
+  "block_height": 603260,
+  "block_index": 1,
+  "hash": "bd6145fcff6709a74f09a2693b901a8256aa3317a7d5451e8b963f434b21a7fb",
+  "hex": "01000000000101246aa2b03b6119f906c6fdb6f00a4e3c215612025c55598ee35e30c2a20aa5dd0100000017160014a0a2bdabb68ee70c43efd20ef24b042f67b5e514ffffffff01b1b701000000000017a914b6bbcc7c70943d1f316390771181a7f2e015cca2870247304402207b230db706897e6dd71b8218292ab00dbc6b63383089ff63300a96ec3fc226e802204aea69af9d18c04a09f5325251678f57f81f7786119b31e8252937c1dd6e7b8e012102cb4acde24be4676b69fcb323bc355c5ba46bb3b1d1e8e505f6074bca3ae418e900000000",
+  "addresses": [
+    "3JMDswjP2JRWwiSd5DtYTUdyesAdzRW4dH",
+    "3NCitnRaGoyBSBveDEe1WrH9ApkZ8Ymw7c"
+  ],
+  "total": 112561,
+  "fees": 89308,
+  "size": 106,
+  "preference": "high",
+  "relayed_by": "72.69.164.7:8333",
+  "confirmed": "2019-11-11T05:52:45Z",
+  "received": "2019-11-11T05:51:18.118Z",
+  "ver": 1,
+  "double_spend": false,
+  "vin_sz": 1,
+  "vout_sz": 1,
+  "confirmations": 1,
+  "confidence": 1,
+  "inputs": [
+    {
+      "prev_hash": "dda50aa2c2305ee38e59555c021256213c4e0af0b6fdc606f919613bb0a26a24",
+      "output_index": 1,
+      "script": "160014a0a2bdabb68ee70c43efd20ef24b042f67b5e514",
+      "output_value": 201869,
+      "sequence": 4294967295,
+      "addresses": [
+        "3NCitnRaGoyBSBveDEe1WrH9ApkZ8Ymw7c"
+      ],
+      "script_type": "pay-to-script-hash",
+      "age": 603255,
+      "witness": [
+        "304402207b230db706897e6dd71b8218292ab00dbc6b63383089ff63300a96ec3fc226e802204aea69af9d18c04a09f5325251678f57f81f7786119b31e8252937c1dd6e7b8e01",
+        "02cb4acde24be4676b69fcb323bc355c5ba46bb3b1d1e8e505f6074bca3ae418e9"
+      ]
+    }
+  ],
+  "outputs": [
+    {
+      "value": 112561,
+      "script": "a914b6bbcc7c70943d1f316390771181a7f2e015cca287",
+      "addresses": [
+        "3JMDswjP2JRWwiSd5DtYTUdyesAdzRW4dH"
+      ],
+      "script_type": "pay-to-script-hash"
+    }
+  ]
+}
